78a348f1dc789ab0eb95f652a3ae2cab9d40a167
CommentaryDev <113671774+CommentaryDev@users.noreply.github.com>
2024-02-24 01:11:58 +0100

    Update page.tsx

--- src/app/bikes/page.tsx
+++ src/app/bikes/page.tsx
@@ -7,7 +7,12 @@ import Link from "next/link";
 import { Swiper ,SwiperSlide } from 'swiper/react';
 import SwiperTrends from '@/components/bikes/SwiperTrends';
 export const dynamic = 'force-dynamic'
+import { revalidatePath } from "next/cache";
 export default async function Bikes()  {
+  async function revalidateNumber() {
+    "use server";
+    revalidatePath("/bikes");
+  }
     const res = await fetch('http://localhost:3000/api/bikes/getTrendingBikes',{ next: { revalidate: 30 } })
     const test = await res.json()
   return (
@@ -17,6 +22,9 @@ export default async function Bikes()  {
         <p className="text-center mb-[30px]">
           The World's Prenium Brands In One Destination
         </p>
+        <form action={revalidateNumber}>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Invalidate cache</button>
+        </form>
         <SwiperTrends test={test}/>
         <Link href='/bikes/all'>
           <button className="btn btn-accent mx-auto">See all bikes</button>
